9e0e42ec92972eb1c4ac8a5e749cc97c58b52c6a
vinyl <vinyl6428@gmail.com>
2024-05-25 09:59:42 +0300

    added sqlite database file exists check

--- pkg/sqlite/sqlite.go
+++ pkg/sqlite/sqlite.go
@@ -2,14 +2,29 @@ package sqlite
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	_ "github.com/mattn/go-sqlite3"
+	"os"
 )
 
 type Connection struct {
 	DB *sql.DB
 }
 
+type DatabaseDoesNotExistError struct {
+	err string
+	url string
+}
+
+func (error *DatabaseDoesNotExistError) Error() string {
+	return fmt.Sprintf("Couldn't connect to database at %s, %s", error.url, error.err)
+}
+
 func New(url string) (*Connection, error) {
+	if _, err := os.Stat(url); errors.Is(err, os.ErrNotExist) {
+		return nil, &DatabaseDoesNotExistError{url: url, err: err.Error()}
+	}
 	db, err := sql.Open("sqlite3", url)
 	if err != nil {
 		return nil, err
